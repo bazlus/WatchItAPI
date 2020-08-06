52c29395a5600903b4d262f08afef9fa3cbfd0ef
Boyan Mihaylov <boyan.mihaylov@tiebreak.solutions>
2020-08-06 18:23:53 +0300

    Initial setup

---
+++ WatchItAPI/WatchIt.API/Features/Base/ApiController.cs
@@ -0,0 +1,10 @@
+﻿namespace WatchIt.API.Features.Base
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class ApiController : ControllerBase
+    {
+    }
+}
---
+++ WatchItAPI/WatchIt.API/Features/Identity/IdentityController.cs
@@ -0,0 +1,14 @@
+﻿namespace WatchIt.API.Features.Identity
+{
+    using Microsoft.AspNetCore.Mvc;
+    using WatchIt.API.Features.Base;
+
+    public class IdentityController : ApiController
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("It Works!");
+        }
+    }
+}
